568e5c772683fa5d60a3e396390068fb6506c791
mharoon-dev <haroon.aghani@gmail.com>
2024-06-29 23:10:00 +0500

    changes

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,18 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
       onwarn: (warning, warn) => {
         // Skip certain warnings
-        if (warning.code === 'THIS_IS_UNDEFINED') return;
+        if (warning.code === "THIS_IS_UNDEFINED") return;
         // Use default for everything else
         warn(warning);
-      }
-    }
-  }
+      },
+    },
+  },
 });
